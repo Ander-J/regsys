16e42d0e8bfe2d216c5ac2d6f15a742ffc10a9ee
Ander-J <anderjors@gmail.com>
2022-07-14 13:30:31 +0300

    Initial commit

---
+++ src/test/java/com/rik/regsys/RegsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rik.regsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
