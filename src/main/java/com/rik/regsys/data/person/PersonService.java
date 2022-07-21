cbb457ecddced3144dabe39df920f8bb2b769e87
Ander-J <anderjors@gmail.com>
2022-07-21 03:37:53 +0300

    Update PersonService to add findById, save and deleteById methods

--- src/main/java/com/rik/regsys/data/person/PersonService.java
+++ src/main/java/com/rik/regsys/data/person/PersonService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,8 +16,21 @@ public class PersonService {
 
     public List<PersonEntity> findAll(){return new ArrayList<>(personRepository.findAll());}
 
+    public PersonEntity findById(Long id) {
+        Optional<PersonEntity> personEntity = personRepository.findById(id);
+        return personEntity.orElse(null);
+    }
+
     public PersonEntity newPerson(PersonEntity person){
         System.out.println(person.toString());
         return personRepository.save(person);
+    }
+
+    public PersonEntity save(PersonEntity personEntity) {
+        return personRepository.save(personEntity);
+    }
+
+    public void deleteById(Long id) {
+        personRepository.deleteById(id);
     }
 }
