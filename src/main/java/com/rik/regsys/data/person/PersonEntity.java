49da701be9b6bd513d24cda502c220464dcc3cf4
Ander-J <anderjors@gmail.com>
2022-07-21 03:37:17 +0300

    Update PersonEntity to add validation

--- src/main/java/com/rik/regsys/data/person/PersonEntity.java
+++ src/main/java/com/rik/regsys/data/person/PersonEntity.java
@@ -1,10 +1,16 @@
 package com.rik.regsys.data.person;
 
 import com.rik.regsys.data.event.EventEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter
@@ -16,24 +22,30 @@ public class PersonEntity {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotEmpty(message = "Eesnimi on puudu!")
     @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
 
+    @NotEmpty(message = "Perenimi on puudu!")
     @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
 
-    @Column(name = "id_code", nullable = false, length = 12)
+    @NotEmpty(message = "Isikukood on puudu!")
+    @Length(min = 11, max = 11, message = "Isikukood on vale!")
+    @Column(name = "id_code", nullable = false, length = 11)
     private String idCode;
 
     @Column(name = "payment_method", nullable = false, length = 50)
     private String paymentMethod;
 
     @Column(name = "extra_info", length = 1500)
+    @Length(max = 1500, message = "Lisainfo kogus on üle limiidi! (maksimum 1500 tähemärki)")
     private String extraInfo;
 
     @ManyToOne
     @JoinColumn(name = "event_entity_id")
     private EventEntity eventEntity;
+
 
     @Override
     public String toString() {
